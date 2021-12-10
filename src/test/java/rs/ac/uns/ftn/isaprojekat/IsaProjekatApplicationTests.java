3ee58d8785efe8efd449c9b275f9661284ce9f8b
Aleksandra <aleksandra.varga@yahoo.com>
2021-12-10 01:43:08 +0100

    initial commit

---
+++ src/test/java/rs/ac/uns/ftn/isaprojekat/IsaProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.isaprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaProjekatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
