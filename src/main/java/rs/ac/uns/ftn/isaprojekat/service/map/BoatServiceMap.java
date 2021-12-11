f61bb86d872dc82682292f4e61badd74b9cfa351
Aleksandra <aleksandra.varga@yahoo.com>
2021-12-12 00:26:20 +0100

    forgot @Service :)

--- src/main/java/rs/ac/uns/ftn/isaprojekat/service/map/BoatServiceMap.java
+++ src/main/java/rs/ac/uns/ftn/isaprojekat/service/map/BoatServiceMap.java
@@ -1,10 +1,12 @@
 package rs.ac.uns.ftn.isaprojekat.service.map;
 
+import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.isaprojekat.model.Boat;
 import rs.ac.uns.ftn.isaprojekat.service.BoatService;
 
 import java.util.Set;
 
+@Service
 public class BoatServiceMap extends AbstractMapService<Boat, Long> implements BoatService {
     @Override
     public Set<Boat> findAll() {
