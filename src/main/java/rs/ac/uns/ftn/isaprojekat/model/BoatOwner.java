4b2daff4714dc583758f8f4a1d704bd858fbfde4
Aleksandra <aleksandra.varga@yahoo.com>
2021-12-19 02:24:28 +0100

    boat and boatOwner mapping

--- src/main/java/rs/ac/uns/ftn/isaprojekat/model/BoatOwner.java
+++ src/main/java/rs/ac/uns/ftn/isaprojekat/model/BoatOwner.java
@@ -1,9 +1,21 @@
 package rs.ac.uns.ftn.isaprojekat.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.Set;
 
 @Entity
 public class BoatOwner extends Person{
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "boatOwner")
+    private Set<Boat> boats;
 
+    public Set<Boat> getBoats() {
+        return boats;
+    }
+
+    public void setBoats(Set<Boat> boats) {
+        this.boats = boats;
+    }
 }
