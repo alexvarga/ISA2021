a9b4294f7d4872167a34606c058db403e9938a83
Aleksandra <aleksandra.varga@yahoo.com>
2022-01-06 17:58:04 +0100

    Validation for search and reservation dates for houses and adventures.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -3,7 +3,6 @@ function validateForm() {
     let y =new Date(document.forms["myForm"]["dateEnd"].value);
     let today = new Date();
     if(x<today || y<today || y<x){
-        console.log("manje od danas")
         document.getElementById('date-error').style.display = 'inline';
         return false;
 
